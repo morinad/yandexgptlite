d411f99240b75770aaa8ad8ae3a172bf0a935d36
Директ-ПРО <44451631+morinad@users.noreply.github.com>
2023-12-19 11:56:03 +0300

    new_params

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yandexgptlite',
-    version='0.1.5',
+    version='0.1.7',
     packages=find_packages(),
     description='Получение ответов от YandexGPT Lite',
     long_description=open('README.md').read(),
